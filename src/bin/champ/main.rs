9a52a6a00049bddf3c908cdc890c21a2572fde0c
Geert Fannes <geert.fannes@gmail.com>
2022-12-30 17:38:50 +0100

    wip

--- src/bin/champ/main.rs
+++ src/bin/champ/main.rs
@@ -1,9 +1,10 @@
 mod cli;
 
-use champetter::error;
+use champetter::error::Result;
 use std::env;
 
 mod data {
+    use champetter::error::{Error, Result};
     use std::fs;
     use std::path;
 
@@ -22,24 +23,42 @@ mod data {
             })
         }
 
-        pub fn create_tree(path: path::PathBuf) -> Nodes {
+        pub fn create_tree(path: path::PathBuf) -> Result<Nodes> {
             println!("{}", path.display());
 
             let mut roots: Nodes = vec![Node::new("/".to_string())];
-            let mut r = &mut roots[0];
+            let mut nodes = &mut roots;
+            let mut ix: usize = 0;
 
             let mut p = path::PathBuf::new();
 
             for part in path.components() {
-                match (part) {
+                match part {
                     path::Component::RootDir => {
                         p.push("/");
                     }
                     path::Component::Normal(str) => {
+                        let mut parent = &mut nodes[ix];
+                        Node::setup(&mut parent.childs, &p)?;
+
                         let name = str.to_string_lossy().to_string();
+                        ix = std::usize::MAX;
+                        for (i, n) in parent.childs.iter().enumerate() {
+                            if n.name == name {
+                                ix = i;
+                                break;
+                            }
+                        }
+                        if ix == std::usize::MAX {
+                            return Err(Box::<dyn std::error::Error + 'static>::from("Not found"));
+                        }
+
                         p.push(&name);
                         println!("{}", p.display());
-                        r.name = name;
+
+                        nodes = &mut parent.childs;
+                        let mut node = &mut nodes[ix];
+                        node.name = name;
 
                         if let Ok(it) = fs::read_dir(&p) {
                             for okrc in it {
@@ -53,14 +72,21 @@ mod data {
                 }
             }
 
-            roots
+            Ok(roots)
+        }
+
+        pub fn setup(nodes: &mut Nodes, path: &path::PathBuf) -> Result<()> {
+            for entry in fs::read_dir(path)? {
+                nodes.push(Node::new(entry?.file_name().to_string_lossy().to_string()))
+            }
+            Ok(())
         }
     }
 }
 
-fn main() -> error::Result<()> {
+fn main() -> Result<()> {
     let path = env::current_dir()?;
-    let root = data::Node::create_tree(path);
+    let root = data::Node::create_tree(path)?;
 
     Ok(())
 }
