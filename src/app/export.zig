cd427adad53982f5dc04bc5b4c4bc2bc5cec8a5c
Geert Fannes <geert.fannes@auro-3d.com>
2026-01-24 09:33:53 +0100

    Initial support for an open task list

--- src/app/export.zig
+++ src/app/export.zig
@@ -14,20 +14,35 @@ const markdown = @import("../markdown.zig");
 pub const Error = error{
     UnexpectedEmptyStack,
     ExpectedAbsolutePath,
+    CouldNotFindSection,
 };
 
 pub const Export = struct {
     const Self = @This();
-    const Stack = std.ArrayList(bool);
+    const BoolStack = std.ArrayList(bool);
+    const NodeIds = std.ArrayList(usize);
+
+    const SectionTasks = struct {
+        const TaskNids = std.ArrayList(usize);
+        section_id: i64,
+        section_nid: usize,
+        task_nids: NodeIds = .{},
+        terms: []const mero.Term = &.{},
+    };
+    const SectionTasksList = std.ArrayList(SectionTasks);
 
     env: Env,
     config: *const cfg.file.Config,
     cli_args: *const cfg.cli.Args,
     forest: *mero.Forest,
-    stack: Stack = .{},
+    stack: BoolStack = .{},
+    sectiontasks_list: SectionTasksList = .{},
 
     pub fn deinit(self: *Self) void {
         self.stack.deinit(self.env.a);
+        for (self.sectiontasks_list.items) |*sectiontasks|
+            sectiontasks.task_nids.deinit(self.env.a);
+        self.sectiontasks_list.deinit(self.env.a);
     }
 
     pub fn call(self: *Self, query_input: [][]const u8) !void {
@@ -43,19 +58,24 @@ pub const Export = struct {
         const Cb = struct {
             const Mode = enum { Search, Write };
 
-            a: std.mem.Allocator,
-            io: std.Io,
+            env: rubr.Env,
             tree: *mero.Tree,
-            stack: *Stack,
+            stack: *BoolStack,
+            sectiontasks_list: *SectionTasksList,
             needle: []const u8,
             output_dir: *std.Io.Dir,
             output: *std.Io.Writer,
             terms: []const mero.Term = &.{},
             section_level: usize = 0,
-            section_id: usize = 0,
+            section_nid_stack: NodeIds = .{},
+            section_id: i64 = -1,
             add_newline_before_bullet: bool = false,
             write_section_id_on_newline: bool = false,
             mode: Mode = .Search,
+
+            fn deinit(my: *@This()) void {
+                my.section_nid_stack.deinit(my.env.a);
+            }
 
             pub fn call(my: *@This(), entry: mero.Tree.Entry, before: bool) !void {
                 const n: *const mero.Node = entry.data;
@@ -65,7 +85,7 @@ pub const Export = struct {
                         switch (n.type) {
                             .Folder => {
                                 if (before) {
-                                    try my.stack.append(my.a, false);
+                                    try my.stack.append(my.env.a, false);
                                 } else {
                                     if (my.stack.pop()) |saw_folder_metadata| {
                                         if (saw_folder_metadata)
@@ -100,10 +120,13 @@ pub const Export = struct {
                     },
                     .Write => {
                         if (n.type == .Section) {
-                            if (before)
-                                my.section_level += 1
-                            else
+                            if (before) {
+                                my.section_level += 1;
+                                try my.section_nid_stack.append(my.env.a, entry.id);
+                            } else {
                                 my.section_level -= 1;
+                                _ = my.section_nid_stack.pop();
+                            }
                         }
 
                         if (!before)
@@ -137,22 +160,36 @@ pub const Export = struct {
 
                                         if (link.image_filepath()) |filepath| {
                                             // Create destination folder
-                                            try my.output_dir.createDirPath(my.io, std.fs.path.dirname(filepath) orelse ".");
+                                            try my.output_dir.createDirPath(my.env.io, std.fs.path.dirname(filepath) orelse ".");
 
                                             const src_folder = std.fs.path.dirname(n.path) orelse return error.ExpectedAbsolutePath;
-                                            var src_dir = try std.Io.Dir.openDirAbsolute(my.io, src_folder, .{});
-                                            defer src_dir.close(my.io);
+                                            var src_dir = try std.Io.Dir.openDirAbsolute(my.env.io, src_folder, .{});
+                                            defer src_dir.close(my.env.io);
 
                                             // &improv: check that we will not overwrite an file with the same relative name
                                             std.debug.print("Coping '{s}' from '{s}'\n", .{ filepath, src_folder });
-                                            try src_dir.copyFile(filepath, my.output_dir.*, filepath, my.io, .{});
+                                            try src_dir.copyFile(filepath, my.output_dir.*, filepath, my.env.io, .{});
                                         }
                                     },
                                     .Newline => {
                                         if (n.type == .Section) {
-                                            try my.output.print(" {{#section:{}}}", .{my.section_id});
                                             my.section_id += 1;
+                                            try my.output.print(" {{#section:{}}}", .{my.section_id});
                                         }
+                                    },
+                                    .Checkbox => {
+                                        const section_nid = my.section_nid_stack.getLastOrNull() orelse {
+                                            try my.env.log.err("Found a checkbok in '{s}' outside a section\n", .{n.path});
+                                            return error.CouldNotFindSection;
+                                        };
+                                        if (rubr.slc.lastPtr(my.sectiontasks_list.items)) |sectiontasks| {
+                                            if (sectiontasks.section_nid != section_nid)
+                                                try my.sectiontasks_list.append(my.env.a, SectionTasks{ .section_id = my.section_id, .section_nid = section_nid, .terms = my.terms });
+                                        } else {
+                                            try my.sectiontasks_list.append(my.env.a, SectionTasks{ .section_id = my.section_id, .section_nid = section_nid, .terms = my.terms });
+                                        }
+                                        const sectiontasks = rubr.slc.lastPtrUnsafe(my.sectiontasks_list.items);
+                                        try sectiontasks.task_nids.append(my.env.a, entry.id);
                                     },
                                     else => {},
                                 }
@@ -172,15 +209,48 @@ pub const Export = struct {
 
         for (needles) |needle| {
             var cb = Cb{
-                .a = self.env.a,
-                .io = self.env.io,
+                .env = self.env,
                 .tree = &self.forest.tree,
                 .stack = &self.stack,
+                .sectiontasks_list = &self.sectiontasks_list,
                 .needle = needle,
                 .output_dir = &output_dir,
                 .output = &output_w.interface,
             };
+            defer cb.deinit();
             try self.forest.tree.dfsAll(&cb);
+        }
+
+        if (!rubr.slc.isEmpty(self.sectiontasks_list.items)) {
+            const w = &output_w.interface;
+            try w.print("\n# Open tasks\n\n", .{});
+            try w.print("This section provides on overview of the open tasks per section in above document.\n\n", .{});
+            for (self.sectiontasks_list.items) |sectiontasks| {
+                {
+                    try w.print("## [", .{});
+                    const n: *const mero.Node = try self.forest.tree.cget(sectiontasks.section_nid);
+                    for (n.line.terms_ixr.begin..n.line.terms_ixr.end) |ix| {
+                        const term = sectiontasks.terms[ix];
+                        switch (term.kind) {
+                            .Section, .Newline => {},
+                            else => try w.print("{s}", .{term.word}),
+                        }
+                    }
+                    try w.print("](#section:{})\n\n", .{sectiontasks.section_id});
+                }
+
+                for (sectiontasks.task_nids.items) |task_nid| {
+                    try w.print("- ", .{});
+                    const n: *const mero.Node = try self.forest.tree.cget(task_nid);
+                    for (n.line.terms_ixr.begin..n.line.terms_ixr.end) |ix| {
+                        const term = sectiontasks.terms[ix];
+                        switch (term.kind) {
+                            .Section, .Bullet, .Checkbox => {},
+                            else => try w.print("{s}", .{term.word}),
+                        }
+                    }
+                }
+            }
         }
 
         try output_w.interface.flush();
